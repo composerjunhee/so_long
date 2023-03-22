ff75ec532c87b9875d485b1c43c501835f1b6193
Kimpozer <composerjunhee@gmail.com>
2023-03-22 19:29:06 +0200

    solong update

--- solongpractice/solong_3rd/image.c
+++ solongpractice/solong_3rd/image.c
@@ -6,7 +6,7 @@
 /*   By: junheeki <junheeki@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:09:51 by junheeki          #+#    #+#             */
-/*   Updated: 2023/03/21 17:04:12 by junheeki         ###   ########.fr       */
+/*   Updated: 2023/03/22 14:53:18 by junheeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_img	img_init(void *mlx)
 	int	wid;
 	int	hei;
 
-	img.land = mlx_xpm_file_to_image(mlx, "images/land.xpm", &wid, &hei);
+	img.land = mlx_xpm_file_to_image(mlx, "images/grass.xpm", &wid, &hei);
 	img.wall = mlx_xpm_file_to_image(mlx, "images/wall.xpm", &wid, &hei);
 	img.collectible = mlx_xpm_file_to_image(mlx, "images/collectible.xpm", &wid, &hei);
 	img.player = mlx_xpm_file_to_image(mlx, "images/woodboy.xpm", &wid, &hei);
